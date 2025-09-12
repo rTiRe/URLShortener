3b9a6b87d68c816689fd8f42b8bf32dae56f5514
TiRe <zientenin@mail.ru>
2025-09-12 16:44:48 +0300

    fix: delete console.log() from logger.ts

--- app/src/logger.ts
+++ app/src/logger.ts
@@ -5,7 +5,4 @@ import env from './config'
 const logger = log4js.getLogger()
 logger.level = log4js.levels.getLevel(env.LOG_LEVEL || 'error') || log4js.levels.ERROR
 
-console.log(env.LOG_LEVEL)
-console.log(`Logger level set to ${logger.level}`)
-
 export default logger
